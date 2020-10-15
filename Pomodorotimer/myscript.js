82754c8d83bcc84149aacfd9c9d6a54bed102941
Meenaakshir <meenaakshivra@gmail.com>
2020-10-15 16:23:16 -0400

    adding javascript files and changes in css html

---
+++ Pomodorotimer/myscript.js
@@ -0,0 +1,37 @@
+function  pomonum() {
+         document.body.style.backgroundColor = "rgb(240, 91, 86)";
+         var pomonum = document.getElementsByClassName("timernumber");
+         pomonum.innerHTML = "25:00";
+}
+function shortbreak() {
+          document.body.style.backgroundColor = "rgb(76, 166, 169)";
+          var shortnum = document.getElementsByClassName("timernumber");
+         /* var sn = document.getElementsByClassName("timernumber");*/
+  shortnum[0].innerHTML = "05:00";
+ 
+
+        }
+function longbreak() {
+        document.body.style.backgroundColor = "#498FC1";
+        var longnum = document.getElementsByClassName("timernumber");
+       longnum[0].innerHTML = "15:00";
+
+}
+
+function actionoftask() {
+  var actn = document.getElementsByClassName("taskmenu");
+  if (actn[0].style.display === "none") {
+    actn[0].style.display = "block";
+  } else {
+    actn[0].style.display = "none";
+  }
+}
+function maillogin(){
+  var login = document.getElementsByClassName("loginframe");
+  if(login[0].style.display === "none") 
+  {
+    login [0].style.display = "block";
+  } else {
+    login [0].style.display = "none";
+  }
+}
